0f2aaef9f94e7d8c221d8edc1e1873cb7d810562
Siyao Shao <shaox146@umn.edu>
2018-04-26 20:39:33 -0500

    Update SS_Assisted_watershed_segmenting.m

--- SS_Assisted_watershed_segmenting.m
+++ SS_Assisted_watershed_segmenting.m
@@ -2,7 +2,7 @@ function [segmented_binary] = SS_Assisted_watershed_segmenting (binaryIM,assisti
 %% Marker control watershed segmentation for binary image with low SNR, the marker generation is through local extended H-minima
 %% of the assisted image. This algorithm is inspired and iterated from Gonzalez and Woods 2008.
 
-BW=mdefilt2(binaryIM,[2,2]);
+BW=medfilt2(binaryIM,[2,2]);
 Pre_marker=imextendedmin(assistingIM,4,8);
 Pre_marker=imfill(Pre_marker,'holes');
 BW=imfill(BW,'holes');
@@ -11,7 +11,7 @@ Image_marker=Pre_marker.*BW;
 
 Lim=watershed(bwdist(Image_marker));
 em=Lim==0;
-f2=~em.*BW_LO_smoothed;
+f2=~em.*BW;
 segmented_binary=logical(f2);
 
 end
